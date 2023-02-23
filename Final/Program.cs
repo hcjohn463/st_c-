28bbef1bc813915474b4a31d060002bf1008fd9e
hcjohn463 <hcjohn463@gmail.com>
2023-02-23 21:06:09 +0800

    final

---
+++ Final/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Final
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string a = "數學：";
+            string f = a.Substring(0, a.Length - 1);
+            Console.WriteLine(f);
+            Console.ReadKey();
+        }
+    }
+}
